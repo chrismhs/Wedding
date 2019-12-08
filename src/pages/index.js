69add13747186c79ff55a49bea6456c82136b628
Chris Hastings-Spital <chrismhs@gmail.com>
2019-12-08 17:06:51 +0000

    Remove page to see if it works

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-// import Welcome from "./welcome"
 import Intro from "./intro"
 import Wedding from "./wedding"
 import Camping from "./camping"
@@ -12,7 +11,7 @@ import Venue from "./venue"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    {/* <Welcome /> */}
+
     <Intro />
     <Wedding />
     <Camping />
